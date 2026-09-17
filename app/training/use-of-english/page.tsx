70fd74f4f6ab97da332a3428b4b8f38b6721f872
margaret8519-byte <margaret8519@gmail.com>
2026-09-18 00:44:21 +0300

    Expose Vzlet 2025 Use of English for Grades 7-8

--- app/training/use-of-english/page.tsx
+++ app/training/use-of-english/page.tsx
@@ -1,9 +1,10 @@
 import GradeOfficialSection from "@/components/training/GradeOfficialSection";
 import { useOfEnglishSet } from "@/data/questions/grade-7-8/2022";
+import { grade78UseOfEnglish2025 } from "@/data/questions/grade-7-8/2025";
 import { grade9UseOfEnglish2022 } from "@/data/questions/grade-9/official/2022";
 import { grade9UseOfEnglish2023 } from "@/data/questions/grade-9/official/2023";
 import { official2024ForGrade } from "@/data/questions/grade-9-10/official-2024";
 
 const grade9UseOfEnglish2024=official2024ForGrade(9)["use-of-english"];
 
-export default function Page(){return <GradeOfficialSection junior={useOfEnglishSet} grade9={[grade9UseOfEnglish2022,grade9UseOfEnglish2023,grade9UseOfEnglish2024]}/>}
+export default function Page(){return <GradeOfficialSection junior={[useOfEnglishSet,grade78UseOfEnglish2025]} grade9={[grade9UseOfEnglish2022,grade9UseOfEnglish2023,grade9UseOfEnglish2024]}/>}
