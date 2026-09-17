72cc6f760dc1733beb381b448c29aa0e0c885b42
margaret8519-byte <margaret8519@gmail.com>
2026-09-17 16:55:38 +0300

    Use shared Grade 9-10 Vzlet 2024 Reading for Grade 9

--- app/training/reading/page.tsx
+++ app/training/reading/page.tsx
@@ -2,5 +2,8 @@ import GradeOfficialSection from "@/components/training/GradeOfficialSection";
 import { readingSet } from "@/data/questions/grade-7-8/2022";
 import { grade9Reading2022 } from "@/data/questions/grade-9/official/2022";
 import { grade9Reading2023 } from "@/data/questions/grade-9/official/2023";
-import { grade9Reading2024 } from "@/data/questions/grade-9/official/2024";
+import { official2024ForGrade } from "@/data/questions/grade-9-10/official-2024";
+
+const grade9Reading2024=official2024ForGrade(9).reading;
+
 export default function Page(){return <GradeOfficialSection junior={readingSet} grade9={[grade9Reading2022,grade9Reading2023,grade9Reading2024]}/>}
