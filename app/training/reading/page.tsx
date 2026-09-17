5ae85f17c9681201863b4df514a2c798e9b6a7c1
margaret8519-byte <margaret8519@gmail.com>
2026-09-18 00:44:16 +0300

    Expose Vzlet 2025 Reading for Grades 7-8

--- app/training/reading/page.tsx
+++ app/training/reading/page.tsx
@@ -1,9 +1,10 @@
 import GradeOfficialSection from "@/components/training/GradeOfficialSection";
 import { readingSet } from "@/data/questions/grade-7-8/2022";
+import { grade78Reading2025 } from "@/data/questions/grade-7-8/2025";
 import { grade9Reading2022 } from "@/data/questions/grade-9/official/2022";
 import { grade9Reading2023 } from "@/data/questions/grade-9/official/2023";
 import { official2024ForGrade } from "@/data/questions/grade-9-10/official-2024";
 
 const grade9Reading2024=official2024ForGrade(9).reading;
 
-export default function Page(){return <GradeOfficialSection junior={readingSet} grade9={[grade9Reading2022,grade9Reading2023,grade9Reading2024]}/>}
+export default function Page(){return <GradeOfficialSection junior={[readingSet,grade78Reading2025]} grade9={[grade9Reading2022,grade9Reading2023,grade9Reading2024]}/>}
