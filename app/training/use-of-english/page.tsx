080774d4975d9460fe2b6ca09d274116cd36f814
margaret8519-byte <margaret8519@gmail.com>
2026-09-17 16:55:47 +0300

    Use shared Grade 9-10 Vzlet 2024 UOE for Grade 9

--- app/training/use-of-english/page.tsx
+++ app/training/use-of-english/page.tsx
@@ -2,5 +2,8 @@ import GradeOfficialSection from "@/components/training/GradeOfficialSection";
 import { useOfEnglishSet } from "@/data/questions/grade-7-8/2022";
 import { grade9UseOfEnglish2022 } from "@/data/questions/grade-9/official/2022";
 import { grade9UseOfEnglish2023 } from "@/data/questions/grade-9/official/2023";
-import { grade9UseOfEnglish2024 } from "@/data/questions/grade-9/official/2024";
+import { official2024ForGrade } from "@/data/questions/grade-9-10/official-2024";
+
+const grade9UseOfEnglish2024=official2024ForGrade(9)["use-of-english"];
+
 export default function Page(){return <GradeOfficialSection junior={useOfEnglishSet} grade9={[grade9UseOfEnglish2022,grade9UseOfEnglish2023,grade9UseOfEnglish2024]}/>}
